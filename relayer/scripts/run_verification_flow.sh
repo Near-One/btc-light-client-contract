e61b65aab37c44780ba2507243002c6198223c55
Julian Kulesh <julian.kulesh@aurora.dev>
2024-06-03 17:26:24 +0800

    chore: remove redundant merkle module and update sample script

--- relayer/scripts/run_verification_flow.sh
+++ relayer/scripts/run_verification_flow.sh
@@ -1,4 +1,5 @@
-
+# Make sure you are running local bitcoin node with a block content downloaded
+# and that block content is not pruned (see Readme for the initial setup)
 
 # Pick a real transaction from a block and verify it
 VERIFY_MODE="true" TRANSACTION_POSITION=0 TRANSACTION_BLOCK_HEIGHT=277136 cargo run
