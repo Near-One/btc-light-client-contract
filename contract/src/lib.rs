4bd0445c7e55f36fa9f7eac7b37930186810a36b
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2024-11-04 14:15:08 +0200

    change u64 cast

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -483,8 +483,9 @@ impl BtcLightClient {
             .get(&interval_tail_header_hash)
             .unwrap_or_else(|| env::panic_str(ERR_KEY_NOT_EXIST));
         let prev_block_time = prev_block_header.block_header.time;
-        let mut actual_time_taken =
-            prev_block_time.saturating_sub(interval_tail_extend_header.block_header.time) as u64;
+        let mut actual_time_taken = u64::from(
+            prev_block_time.saturating_sub(interval_tail_extend_header.block_header.time),
+        );
 
         if actual_time_taken < EXPECTED_TIME / MAX_ADJUSTMENT_FACTOR {
             actual_time_taken = EXPECTED_TIME / MAX_ADJUSTMENT_FACTOR;
