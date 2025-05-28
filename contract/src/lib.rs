7fb3d226c237917f6c338400b115e2f89af5e08f
kiseln <3428059+kiseln@users.noreply.github.com>
2025-05-28 21:25:10 +0200

    Refactor zcash target calculation to be closer to reference code

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -592,7 +592,35 @@ impl BtcLightClient {
         &self,
         block_header: &Header,
         prev_block_header: &ExtendedHeader,
-    ) {
+    ) { 
+        let expected_bits = self.zcash_get_next_work_required(block_header, prev_block_header);
+
+        require!(
+            expected_bits == block_header.bits,
+            format!(
+                "Error: Incorrect target. Expected bits: {:?}, Actual bits: {:?}",
+                expected_bits, block_header.bits
+            )
+        );
+
+        // Check Equihash solution
+        let n = 200;
+        let k = 9;
+        let input = block_header.get_block_header_vec_for_equihash();
+
+        equihash::is_valid_solution(n, k, &input, &block_header.nonce.0, &block_header.solution)
+            .unwrap_or_else(|e| {
+                env::panic_str(&format!("Invalid Equihash solution: {}", e));
+            });
+    }
+
+    // Reference implementation: https://github.com/zcash/zcash/blob/v6.2.0/src/pow.cpp#L20
+    #[cfg(feature = "zcash")]
+    fn zcash_get_next_work_required(
+        &self,
+        block_header: &Header,
+        prev_block_header: &ExtendedHeader,
+    ) -> u32 {
         use btc_types::network::ZCASH_MEDIAN_TIME_SPAN;
 
         let config = self.get_config();
@@ -600,22 +628,16 @@ impl BtcLightClient {
         if let Some(pow_allow_min_difficulty_blocks_after_height) =
             config.pow_allow_min_difficulty_blocks_after_height
         {
-            // Special difficulty rule for testnet:
-            // If the new block's timestamp is more than 6 * block interval minutes
-            // then allow mining of a min-difficulty block.
+            // Comparing with >= because this function returns the work required for the block after prev_block_header
             if prev_block_header.block_height >= pow_allow_min_difficulty_blocks_after_height {
+                // Special difficulty rule for testnet:
+                // If the new block's timestamp is more than 6 * block interval minutes
+                // then allow mining of a min-difficulty block.
                 if i64::from(block_header.time)
                     > i64::from(prev_block_header.block_header.time)
                         + config.pow_target_spacing(prev_block_header.block_height + 1) * 6
                 {
-                    let expected_bits = config.proof_of_work_limit_bits;
-                    require!(
-                        expected_bits == block_header.bits,
-                        format!(
-                            "Error: Incorrect target. Expected bits: {:?}, Actual bits: {:?}",
-                            expected_bits, block_header.bits
-                        )
-                    );
+                    return config.proof_of_work_limit_bits;
                 }
             }
         }
@@ -653,8 +675,32 @@ impl BtcLightClient {
             median_time[median_time.len() / 2]
         };
 
+        // The protocol specification leaves MeanTarget(height) as a rational, and takes the floor
+        // only after dividing by AveragingWindowTimespan in the computation of Threshold(height):
+        // <https://zips.z.cash/protocol/protocol.pdf#diffadjustment>
+        //
+        // Here we take the floor of MeanTarget(height) immediately, but that is equivalent to doing
+        // so only after a further division, as proven in <https://math.stackexchange.com/a/147832/185422>.
         let average_target = total_target / U256::from(config.pow_averaging_window as u64);
-        let next_height = prev_block_header.block_height + 1;
+
+        return self.zcash_calculate_next_work_required(
+            average_target,
+            prev_block_median_time_past,
+            first_block_in_interval_median_time_past,
+            prev_block_header.block_height + 1,
+        )
+    }
+
+    #[cfg(feature = "zcash")]
+    fn zcash_calculate_next_work_required(
+        &self,
+        average_target: U256,
+        last_interval_block_median_time_past: u32,
+        first_interval_block_median_time_past: u32,
+        next_height: u64,
+    ) -> u32 {
+        let config = self.get_config();
+
         let averaging_window_timespan = config.averaging_window_timespan(next_height);
         let min_actual_timespan = config.min_actual_timespan(next_height);
         let max_actual_timespan = config.max_actual_timespan(next_height);
@@ -662,7 +708,7 @@ impl BtcLightClient {
         // Limit adjustment step
         // Use medians to prevent time-warp attacks
         let mut actual_timespan: i64 =
-            (prev_block_median_time_past - first_block_in_interval_median_time_past).into();
+            (last_interval_block_median_time_past - first_interval_block_median_time_past).into();
 
         actual_timespan =
             averaging_window_timespan + (actual_timespan - averaging_window_timespan) / 4;
@@ -684,25 +730,7 @@ impl BtcLightClient {
             new_target = config.pow_limit;
         }
 
-        let expected_bits = new_target.target_to_bits();
-
-        require!(
-            expected_bits == block_header.bits,
-            format!(
-                "Error: Incorrect target. Expected bits: {:?}, Actual bits: {:?}",
-                expected_bits, block_header.bits
-            )
-        );
-
-        // Check Equihash solution
-        let n = 200;
-        let k = 9;
-        let input = block_header.get_block_header_vec_for_equihash();
-
-        equihash::is_valid_solution(n, k, &input, &block_header.nonce.0, &block_header.solution)
-            .unwrap_or_else(|e| {
-                env::panic_str(&format!("Invalid Equihash solution: {}", e));
-            });
+        new_target.target_to_bits()
     }
 
     fn get_prev_header(&self, current_header: &ExtendedHeader) -> ExtendedHeader {
