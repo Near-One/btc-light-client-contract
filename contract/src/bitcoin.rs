028c9123b124b264a6a8359b6daa629e13aade24
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2025-06-05 17:56:06 +0100

    move fn

--- contract/src/bitcoin.rs
+++ contract/src/bitcoin.rs
@@ -1,13 +1,15 @@
 use crate::utils::BlocksGetter;
 use crate::{BtcLightClient, BtcLightClientExt, Header, U256};
 use btc_types::header::ExtendedHeader;
+use btc_types::network::NetworkConfig;
 use btc_types::utils::target_from_bits;
 use near_sdk::{near, require};
 
 #[near]
 impl BtcLightClient {
     pub(crate) fn check_pow(&self, block_header: &Header, prev_block_header: &ExtendedHeader) {
-        let expected_bits = self.get_next_work_required(block_header, prev_block_header);
+        let config = self.get_config();
+        let expected_bits = get_next_work_required(&config, block_header, prev_block_header, self);
 
         require!(
             expected_bits == block_header.bits,
@@ -17,76 +19,75 @@ impl BtcLightClient {
             )
         );
     }
+}
 
-    //https://github.com/bitcoin/bitcoin/blob/ae024137bda9fe189f4e7ccf26dbaffd44cbbeb6/src/pow.cpp#L14
-    fn get_next_work_required(
-        &self,
-        block_header: &Header,
-        prev_block_header: &ExtendedHeader,
-    ) -> u32 {
-        let config = self.get_config();
-
-        if (prev_block_header.block_height + 1) % config.difficulty_adjustment_interval != 0 {
-            if config.pow_allow_min_difficulty_blocks {
-                if block_header.time
-                    > prev_block_header.block_header.time + 2 * config.pow_target_spacing
-                {
-                    return config.proof_of_work_limit_bits;
-                }
-
-                let mut current_block_header = prev_block_header.clone();
-                while current_block_header.block_header.bits == config.proof_of_work_limit_bits
-                    && current_block_header.block_height % config.difficulty_adjustment_interval
-                        != 0
-                {
-                    current_block_header = self.get_prev_header(&current_block_header.block_header);
-                }
-
-                let last_bits = current_block_header.block_header.bits;
-                return last_bits;
+//https://github.com/bitcoin/bitcoin/blob/ae024137bda9fe189f4e7ccf26dbaffd44cbbeb6/src/pow.cpp#L14
+fn get_next_work_required(
+    config: &NetworkConfig,
+    block_header: &Header,
+    prev_block_header: &ExtendedHeader,
+    blocks_getter: &impl BlocksGetter,
+) -> u32 {
+    if (prev_block_header.block_height + 1) % config.difficulty_adjustment_interval != 0 {
+        if config.pow_allow_min_difficulty_blocks {
+            if block_header.time
+                > prev_block_header.block_header.time + 2 * config.pow_target_spacing
+            {
+                return config.proof_of_work_limit_bits;
             }
-            return prev_block_header.block_header.bits;
+
+            let mut current_block_header = prev_block_header.clone();
+            while current_block_header.block_header.bits == config.proof_of_work_limit_bits
+                && current_block_header.block_height % config.difficulty_adjustment_interval != 0
+            {
+                current_block_header =
+                    blocks_getter.get_prev_header(&current_block_header.block_header);
+            }
+
+            let last_bits = current_block_header.block_header.bits;
+            return last_bits;
         }
-
-        let first_block_height =
-            prev_block_header.block_height - (config.difficulty_adjustment_interval - 1);
-
-        let interval_tail_extend_header = self.get_header_by_height(first_block_height);
-        self.calculate_next_work_required(
-            prev_block_header,
-            interval_tail_extend_header.block_header.time.into(),
-        )
+        return prev_block_header.block_header.bits;
     }
 
-    //https://github.com/bitcoin/bitcoin/blob/ae024137bda9fe189f4e7ccf26dbaffd44cbbeb6/src/pow.cpp#L50
-    fn calculate_next_work_required(
-        &self,
-        prev_block_header: &ExtendedHeader,
-        first_block_time: i64,
-    ) -> u32 {
-        let config = self.get_config();
-        let prev_block_time: i64 = prev_block_header.block_header.time.into();
+    let first_block_height =
+        prev_block_header.block_height - (config.difficulty_adjustment_interval - 1);
 
-        let mut actual_time_taken: i64 = prev_block_time - first_block_time;
-        if actual_time_taken < config.pow_target_timespan / 4 {
-            actual_time_taken = config.pow_target_timespan / 4;
-        }
-        if actual_time_taken > config.pow_target_timespan * 4 {
-            actual_time_taken = config.pow_target_timespan * 4;
-        }
+    let interval_tail_extend_header = blocks_getter.get_header_by_height(first_block_height);
+    calculate_next_work_required(
+        config,
+        prev_block_header,
+        interval_tail_extend_header.block_header.time.into(),
+    )
+}
 
-        let new_target = target_from_bits(prev_block_header.block_header.bits);
+//https://github.com/bitcoin/bitcoin/blob/ae024137bda9fe189f4e7ccf26dbaffd44cbbeb6/src/pow.cpp#L50
+fn calculate_next_work_required(
+    config: &NetworkConfig,
+    prev_block_header: &ExtendedHeader,
+    first_block_time: i64,
+) -> u32 {
+    let prev_block_time: i64 = prev_block_header.block_header.time.into();
 
-        let (mut new_target, new_target_overflow) =
-            new_target.overflowing_mul(<i64 as TryInto<u64>>::try_into(actual_time_taken).unwrap());
-        require!(!new_target_overflow, "new target overflow");
-        new_target = new_target
-            / U256::from(<i64 as TryInto<u64>>::try_into(config.pow_target_timespan).unwrap());
-
-        if new_target > config.pow_limit {
-            new_target = config.pow_limit;
-        }
-
-        new_target.target_to_bits()
+    let mut actual_time_taken: i64 = prev_block_time - first_block_time;
+    if actual_time_taken < config.pow_target_timespan / 4 {
+        actual_time_taken = config.pow_target_timespan / 4;
     }
+    if actual_time_taken > config.pow_target_timespan * 4 {
+        actual_time_taken = config.pow_target_timespan * 4;
+    }
+
+    let new_target = target_from_bits(prev_block_header.block_header.bits);
+
+    let (mut new_target, new_target_overflow) =
+        new_target.overflowing_mul(<i64 as TryInto<u64>>::try_into(actual_time_taken).unwrap());
+    require!(!new_target_overflow, "new target overflow");
+    new_target = new_target
+        / U256::from(<i64 as TryInto<u64>>::try_into(config.pow_target_timespan).unwrap());
+
+    if new_target > config.pow_limit {
+        new_target = config.pow_limit;
+    }
+
+    new_target.target_to_bits()
 }
