4deb7a301b1b44638046e9ca66027e11de1d4170
Karim <karim.alabtakh@nearone.org>
2025-08-20 17:34:13 +0100

    chore: add more checks for block (#115)
    * Zcash: add timestamps verification
    
    * Remove irrelevant comment
    
    * Fix error message
    
    * Fix typo
    
    * Add timestamp & version verification for bitcoin
    
    * litecoin: add timestamp verification
    
    * Align error message

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -381,7 +381,7 @@ impl BtcLightClient {
         #[cfg(feature = "zcash")]
         {
             require!(
-                btc_types::network::ZCASH_MEDIAN_TIME_SPAN
+                btc_types::network::MEDIAN_TIME_SPAN
                     + usize::try_from(config.pow_averaging_window).unwrap()
                     == submit_blocks.len() - 1,
                 "ERR_NOT_ENOUGH_BLOCKS_FOR_ZCASH"
