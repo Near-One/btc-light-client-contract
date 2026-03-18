467ddb4c63ededa804291affc543566df5d38566
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2026-03-18 22:44:45 +0000

    Extra script_sig checks on Dogecoin Blocks submission  (#116)
    * add extra checks
    
    * parse nonce and size
    
    * extract expected_index
    
    * safe aux chain id
    
    * fix tests
    
    * fix relayer
    
    * mul with overflow
    
    * fmt
    
    * clippy
    
    * fmt
    
    * find -> contains
    
    * fix clippy
    
    * fix tests
    
    * remove aux_parent_block and check target
    
    * move target check
    
    * fix check target
    
    * fix error msg
    
    * move aux_chain_id to network config
    
    * add timestamps checks to dogecoin
    
    * add dogecoin test
    
    * tests for dogecoin
    
    * fix bitcoin tests
    
    * fmt
    
    * fix tests
    
    * clippy
    
    * clippy
    
    * fmt
    
    * clippy revert
    
    * zcash test fix
    
    * fix relayer
    
    * fix reorg test
    
    * remove redundant cycles
    
    * fmt

--- contract/src/utils.rs
+++ contract/src/utils.rs
@@ -16,8 +16,8 @@ pub fn get_median_time_past(
     let mut median_time = [0u32; MEDIAN_TIME_SPAN];
     let mut current_header = block_header;
 
-    for i in 0..MEDIAN_TIME_SPAN {
-        median_time[i] = current_header.block_header.time;
+    for slot in &mut median_time {
+        *slot = current_header.block_header.time;
         current_header = prev_block_getter.get_prev_header(&current_header.block_header);
     }
 
